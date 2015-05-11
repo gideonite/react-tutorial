bbf400f50ff5bd1204f2254c22cad7800debb163
Gideon Dresdner <gideond@gmail.com>
2015-05-11 05:11:17 +0300

    I know how to hook in data.

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,3 +1,7 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
 var Comment = React.createClass({
   render: function() {
     var raw_markup = marked(this.props.children.toString(), {santize: true});
@@ -17,7 +21,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data}/>
         <CommentForm />
       </div>
     );
@@ -26,10 +30,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment){
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt"> This is one comment </Comment>
-        <Comment author="Jordan Walke"> This is another comment </Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -44,5 +54,5 @@ var CommentForm = React.createClass({
   }
 });
 
-React.render(<CommentBox />,
+React.render(<CommentBox data={data}/>,
              document.getElementById('content'));
